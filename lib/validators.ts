870d2a191843102cdafade48f577b2a0bec47e3a
Ling Fung Chan <justinlfchan@gmail.com>
2025-06-05 09:06:22 +0100

    Woeking on sign-in action and sign-in page with callback url

--- lib/validators.ts
+++ lib/validators.ts
@@ -26,5 +26,5 @@ export const insertProductSchema = z.object({
 // Schema for signing users in
 export const signInFormSchema = z.object({
   email: z.string().email("Invalid email address"),
-  passord: z.string().min(6, "Password must be at least 6 characters."),
+  password: z.string().min(6, "Password must be at least 6 characters."),
 });
