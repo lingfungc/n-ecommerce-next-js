3346552d8619cdac22614c8e06b5454cb72ca4c0
Ling Fung Chan <justinlfchan@gmail.com>
2025-06-13 15:03:38 +0100

    Finished section 8 for Paypal payments

---
+++ follow-ups.md
@@ -0,0 +1,23 @@
+### Follow Ups ###
+
+
+# 1 Order Reference Number ( 11 JUN )
+
+- Implement a simple order reference number to Order Prisma schema
+
+
+# 2 Check Shopify E-Commerce Normal Practices ( 11 JUN )
+
+- Terms + Conditions
+
+- Private Policy
+
+- and so on ...
+
+
+# 3 Production: Error Handling + Logging
+
+
+# 4 PayPal Setup
+
+- Set currency to GBP and test the API calls
--- lib/actions/user.actions.ts
+++ lib/actions/user.actions.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import {
+  paymentMethodSchema,
   shippingAddressSchema,
   signInFormSchema,
   signUpFormSchema,
@@ -117,7 +118,35 @@ export async function updateUserAddress(data: ShippingAddress) {
 
     return {
       success: true,
-      message: "User updated successfully",
+      message: "User's address updated successfully",
+    };
+  } catch (error) {
+    return { success: false, message: formatError(error) };
+  }
+}
+
+// Update user's payment method
+export async function updateUserPaymentMethod(
+  data: z.infer<typeof paymentMethodSchema>
+) {
+  try {
+    const session = await auth();
+    const currentUser = await prisma.user.findFirst({
+      where: { id: session?.user?.id },
+    });
+
+    if (!currentUser) throw new Error("User not found");
+
+    const paymentMethod = paymentMethodSchema.parse(data);
+
+    await prisma.user.update({
+      where: { id: currentUser.id },
+      data: { paymentMethod: paymentMethod.type },
+    });
+
+    return {
+      success: true,
+      message: "User's payment method updated successfully",
     };
   } catch (error) {
     return { success: false, message: formatError(error) };
